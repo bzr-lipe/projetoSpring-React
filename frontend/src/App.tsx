4322a511d1565ec9239fb80476d54683bf3f187c
bzr.lipe <bzr.lipe@gmail.com>
2022-09-08 14:51:29 -0300

    Other Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
 import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
+
 
 function App() {
-return (
-  <>
-    <h1>teste</h1>
-    <NotificationButton/>
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+          <SalesCard/>
+          </div>
+        </section>
+      </main>
     </>
   )
 
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/img/image 1.svg'
+
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+        <div className="dsmeta-logo-container ">
+            <img src={logo} alt="Logo empresa"/>
+            <h1>DSMeta</h1>
+            <p>
+                Desenvolvido por <a href="https://github.com/bzr-lipe" target="_blank" className="text-light">Felipe Bezerra</a>
+            </p>
+        </div>
+    </header>
+    )
+}
+
+export default Header
