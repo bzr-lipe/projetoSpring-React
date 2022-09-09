7456ff02f05659f2b5523ff512b48f76c8cebb23
bzr.lipe <bzr.lipe@gmail.com>
2022-09-09 10:34:33 -0300

    Date select

--- backend/src/main/java/com/felipebezerra/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/felipebezerra/dsmeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.felipebezerra.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.felipebezerra.dsmeta.entities.Sale;
@@ -15,7 +19,19 @@ public class SaleService {
 	@Autowired
 	private SalesRepository repository;
 	
-	public List<Sale> findSales() {
-		return repository.findAll();
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+			
+		
+		
+		
+		
+		
+		return repository.findSales(min, max, pageable);
 		}
 }
