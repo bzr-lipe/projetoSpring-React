8a33d4e1a8fd4f2634263966c533ec37f7fd63be
bzr.lipe <bzr.lipe@gmail.com>
2022-09-11 20:03:19 -0300

    sale listing

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -5,20 +5,24 @@ import "react-datepicker/dist/react-datepicker.css";
 import './styles.css'
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { BASE_URL } from '../../utils/request';
+import { Sale } from '../../models/sale';
 
 function SalesCard() {
 
-    const min= new Date(new Date().setDate(new Date().getDate() - 365));
-    const max= new Date();
+    const min = new Date(new Date().setDate(new Date().getDate() - 365));
+    const max = new Date();
 
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
 
+    const [sales, setSales] = useState<Sale[]>([]);
+
     useEffect(() => {
-       axios.get("http://localhost:8080/sales")
-       .then(response => {
-        console.log(response.data)
-       })
+        axios.get(`${BASE_URL}/sales`)
+            .then(response => {
+                setSales(response.data.content);
+            })
     }, [])
 
 
@@ -59,39 +63,22 @@ function SalesCard() {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">01/05/2002</td>
-                                <td>Anakin</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 553000.00</td>
-                                <td>
-                                    <NotificationButton />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">01/02/2002</td>
-                                <td>Anakin</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 553000.00</td>
-                                <td>
-                                    <NotificationButton />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">01/02/2002</td>
-                                <td>Anakin</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 553000.00</td>
-                                <td>
-                                    <NotificationButton />
-                                </td>
-                            </tr>
+                            {sales.map(sale => {
+                                return (
+                                    <tr key={sale.id}>
+                                        <td className="show992">#{sale.id}</td>
+                                        <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                                        <td>{sale.sellerName}</td>
+                                        <td className="show992">{sale.visited}</td>
+                                        <td className="show992">{sale.deals}</td>
+                                        <td>R$ {sale.amount.toFixed(2)}</td>
+                                        <td>
+                                            <NotificationButton />
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                            }
                         </tbody>
                     </table>
                 </div>
